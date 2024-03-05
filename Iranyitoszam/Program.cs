27c4e5b65a011fcf415d6e451442a388c3135992
SzilagyiZsolt <zsoltika20030@gmail.com>
2024-03-05 10:43:31 +0100

    Add project files.

---
+++ Iranyitoszam/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Iranyitoszam;
+
+namespace Iranyitoszam
+{
+    internal class Program
+    {
+        static PostalCode postalCode = null;
+        static async Task Main(string[] args)
+        {
+            string keresettIranyitoszam = "4032";
+            await telepulesAdatok(keresettIranyitoszam);
+            Console.WriteLine($"{postalCode.Places[0].PlaceName} település irányítószáma: {keresettIranyitoszam}");
+            Console.WriteLine($"{postalCode.Places[0].State} megye");
+            Console.WriteLine($" Google Maps link: https://www.google.com/maps/place/{postalCode.Places[0].Latitude},{postalCode.Places[0].Longitude}");
+            Console.ReadLine();
+        }
+        private static async Task telepulesAdatok(string irszam)
+        {
+            string url = $"http://api.zippopotam.us/hu/{irszam}";
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+            var response = await client.SendAsync(request);
+            if(!response.IsSuccessStatusCode)
+            {
+                throw new Exception("Hiba a lekérdezés során");
+            }
+            //response.EnsureSuccessStatusCode();
+            string jsonString = await response.Content.ReadAsStringAsync();
+            postalCode = PostalCode.FromJson(jsonString);
+        }
+    }
+}
---
+++ ValutaValto/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using ValutaValto;
+
+namespace ValutaValto
+{
+    internal class Program
+    {
+        static valutavaltas valto = null;
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Mennyit szeretnél átváltani?");
+            int forint=int.Parse(Console.ReadLine());
+            await Valto();
+            var euro = forint/valto.Rates["HUF"];
+            var dollar = (forint/valto.Rates["HUF"])*valto.Rates["USD"];
+            Console.WriteLine($"{forint}ft={euro}{valto.Base}");
+            Console.WriteLine($"{forint}ft={dollar}USD");
+            Console.ReadKey();
+        }
+
+        private static async Task Valto()
+        {
+            string url = $"https://infojegyzet.hu/webszerkesztes/php/valuta/api/v1/arfolyam/";
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+            var response = await client.SendAsync(request);
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("Hiba a lekérdezés során");
+            }
+            string jsonString = await response.Content.ReadAsStringAsync();
+            valto = valutavaltas.FromJson(jsonString);
+        }
+    }
+}
